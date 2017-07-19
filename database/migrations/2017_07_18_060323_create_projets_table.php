87dce04a42c81b76edbcc5e53a025159f2ecde6d
Lovepreet Singh <preetacc@gmail.com>
2017-07-19 22:19:02 +0530

    New Migration & Seeders

---
+++ database/migrations/2017_07_18_060323_create_projets_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('owner')->unsigned();
+            $table->timestamps();
+            $table->longText('detail');
+            $table->json('setting');
+            $table->json('team');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
