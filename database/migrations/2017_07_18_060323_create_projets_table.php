fe7b172047ed56f42a0e7ebe4bd6f1cd4d9d19b9
Lovepreet Singh <preetacc@gmail.com>
2017-07-19 22:55:22 +0530

    Timestamp Default Value

--- database/migrations/2017_07_18_060323_create_projets_table.php
+++ database/migrations/2017_07_18_060323_create_projets_table.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
@@ -17,7 +18,8 @@ class CreateProjetsTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->integer('owner')->unsigned();
-            $table->timestamps();
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
             $table->longText('detail');
             $table->json('setting');
             $table->json('team');
